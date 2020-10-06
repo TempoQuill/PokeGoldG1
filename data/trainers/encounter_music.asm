6a625ef7770cde78630394509a77ad94586b65d4
TempoQuill <57678934+TempoQuill@users.noreply.github.com>
2020-10-06 16:56:16 -0400

    Add files via upload

---
+++ data/trainers/encounter_music.asm
@@ -0,0 +1,75 @@
+; What music plays when a trainer notices you
+
+TrainerEncounterMusic::
+; entries correspond to trainer classes (see constants/trainer_constants.asm)
+	db MUSIC_MANIAC ; none
+	db MUSIC_YOUTH  ; falkner
+	db MUSIC_GIRL   ; whitney
+	db MUSIC_YOUTH  ; bugsy
+	db MUSIC_YOUTH  ; morty
+	db MUSIC_YOUTH  ; pryce
+	db MUSIC_GIRL   ; jasmine
+	db MUSIC_YOUTH  ; chuck
+	db MUSIC_GIRL   ; clair
+	db MUSIC_RIVAL  ; rival1
+	db MUSIC_MANIAC ; pokemon_prof
+	db MUSIC_MANIAC ; will
+	db MUSIC_MANIAC ; cal
+	db MUSIC_MANIAC ; bruno
+	db MUSIC_GIRL   ; karen
+	db MUSIC_MANIAC ; koga
+	db MUSIC_YOUTH  ; champion
+	db MUSIC_YOUTH  ; brock
+	db MUSIC_GIRL   ; misty
+	db MUSIC_YOUTH  ; lt_surge
+	db MUSIC_MANIAC ; scientist
+	db MUSIC_GIRL   ; erika
+	db MUSIC_YOUTH  ; youngster
+	db MUSIC_YOUTH  ; schoolboy
+	db MUSIC_YOUTH  ; bird_keeper
+	db MUSIC_GIRL   ; lass
+	db MUSIC_GIRL   ; janine
+	db MUSIC_YOUTH  ; cooltrainerm
+	db MUSIC_GIRL   ; cooltrainerf
+	db MUSIC_GIRL   ; beauty
+	db MUSIC_MANIAC ; pokemaniac
+	db MUSIC_MANIAC ; gruntm
+	db MUSIC_MANIAC ; gentleman
+	db MUSIC_GIRL   ; skier
+	db MUSIC_GIRL   ; teacher
+	db MUSIC_GIRL   ; sabrina
+	db MUSIC_YOUTH  ; bug_catcher
+	db MUSIC_YOUTH  ; fisher
+	db MUSIC_YOUTH  ; swimmerm
+	db MUSIC_GIRL   ; swimmerf
+	db MUSIC_YOUTH  ; sailor
+	db MUSIC_MANIAC ; super_nerd
+	db MUSIC_RIVAL  ; rival2
+	db MUSIC_YOUTH  ; guitarist
+	db MUSIC_YOUTH  ; hiker
+	db MUSIC_YOUTH  ; biker
+	db MUSIC_YOUTH  ; blaine
+	db MUSIC_MANIAC ; burglar
+	db MUSIC_MANIAC ; firebreather
+	db MUSIC_YOUTH  ; juggler
+	db MUSIC_MANIAC ; blackbelt_t
+	db MUSIC_MANIAC ; executivem
+	db MUSIC_YOUTH  ; psychic_t
+	db MUSIC_GIRL   ; picnicker
+	db MUSIC_YOUTH  ; camper
+	db MUSIC_MANIAC ; executivef
+	db MUSIC_MANIAC ; sage
+	db MUSIC_MANIAC ; medium
+	db MUSIC_YOUTH  ; boarder
+	db MUSIC_YOUTH  ; pokefanm
+	db MUSIC_GIRL   ; kimono_girl
+	db MUSIC_GIRL   ; twins
+	db MUSIC_GIRL   ; pokefanf
+	db MUSIC_YOUTH  ; red
+	db MUSIC_RIVAL  ; blue
+	db MUSIC_YOUTH  ; officer
+	db MUSIC_MANIAC ; gruntf
+	db MUSIC_MANIAC ; mysticalman
+	db MUSIC_MANIAC
+	db MUSIC_MANIAC
+	db MUSIC_MANIAC
---
+++ data/wild/swarm_water.asm
@@ -0,0 +1,12 @@
+; Pokémon swarms in water
+
+SwarmWaterWildMons:
+
+; Marill swarm
+	map_id MOUNT_MORTAR_1F_OUTSIDE
+	db 4 percent ; encounter rate
+	db 20, GOLDEEN
+	db 20, MARILL
+	db 20, SEAKING
+
+	db -1 ; end
